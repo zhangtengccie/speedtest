dc0929e40981ba7789c772d13e8a42abfe623dc1
zhangtengccie <zhangtengccie@gamil.com>
2021-06-26 22:44:32 +0800

    Initial commit

---
+++ speedtest_bandwidth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3.9
+# -*- coding=utf8 -*- 
+# Creater:zhangteng
+import speedtest
+import urllib.request
+import tkinter as tk
+
+class Application(tk.Tk):
+    def __init__(self):
+        super.__init__()
+        self.title('测试网速')
+        self.geometry('400x200')
+
+
+
+
+
+s = speedtest.Speedtest()
+server = s.get_servers()
+print(server)
+server_list = s.get_best_server()
+
+print(server_list)
+dr = s.download()
+
+up = s.upload()
+print(f'下载速度：{dr /1024 /1024:.2f} Mbits')
+print(f'上次速度：{up /1024 /1024:.2f} Mbits')
+
